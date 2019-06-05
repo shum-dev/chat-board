d58713c95c87a06fcda3cbb6920f80b98fb7e0f4
Egor Shum <89135509130@mail.ru>
2019-06-05 15:05:40 +0700

    Add signIn logic

--- chat-board-server/handlers/auth.js
+++ chat-board-server/handlers/auth.js
@@ -1,7 +1,41 @@
 const db = require("../models");
 const jwt = require("jsonwebtoken");
 
-exports.signin = function(){}
+exports.signin = async function(req, res, next) {
+  try{
+    let user = await db.User.findOne({
+      email: req.body.email
+    });
+    let { id, username, profileImageUrl } = user;
+    let isMatch = await user.comparePassword(req.body.password);
+    if(isMatch){
+      let token = jwt.sign(
+        {
+          id,
+          username,
+          profileImageUrl
+        },
+        process.env.SECRET_KEY
+      );
+      return res.status(200).json({
+        id,
+        username,
+        profileImageUrl,
+        token
+      })
+    } else {
+      return next({
+        status: 400,
+        message: "Invalid Email/Password pair1."
+      })
+    }
+  } catch(err) {
+    return next({
+      status: 400,
+      message: "Invalid Email/Password pair2."
+    })
+  }
+}
 
 exports.signup = async function(req, res, next) {
   try {
