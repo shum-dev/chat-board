2a90bf7310f59cdbedacee2c44cecc0c620dfa0a
Egor Shum <89135509130@mail.ru>
2019-06-06 17:18:28 +0700

    Connect bootstrap. Create generic Navbar container and add it to the App

--- chat-board-client/src/containers/App.js
+++ chat-board-client/src/containers/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Provider } from "react-redux";
 import { configureStore } from "../store";
 import { BrowserRouter as Router } from "react-router-dom";
+import Navbar from "./Navbar";
 
 const store = configureStore();
 
@@ -9,7 +10,9 @@ const App = () => {
   return (
     <Provider store={store}>
       <Router>
-        <div>Hello world!</div>
+        <div className="onboarding">
+          <Navbar />
+        </div>
       </Router>
     </Provider>
   );
