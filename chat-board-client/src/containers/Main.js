2d13add736c7609645f67dc55f99d7e11ad0c696
Egor Shum <89135509130@mail.ru>
2019-06-10 10:28:42 +0700

    Implement 'display SignIn/SignUp Errors' feature.

--- chat-board-client/src/containers/Main.js
+++ chat-board-client/src/containers/Main.js
@@ -3,21 +3,35 @@ import { Switch, Route, withRouter, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 import Homepage from "../components/Homepage";
 import AuthForm from "../components/AuthForm";
-import { authUser } from "../store/actions/auth"
+import { authUser } from "../store/actions/auth";
+import { removeError } from "../store/actions/errors";
 
-const Main = ({authUser}) => {
+const Main = ({authUser, errors, removeError}) => {
   return (
     <div className="container">
       <Switch>
         <Route exact path="/" render={props => <Homepage {...props} />} />
         <Route exact path="/signin" render={props => {
           return(
-            <AuthForm {...props} buttonText="Log in" heading="Wellcome Back." onAuth={authUser} />
+            <AuthForm
+              {...props}
+              buttonText="Log in"
+              heading="Wellcome Back."
+              onAuth={authUser}
+              errors={errors}
+              removeError={removeError} />
           )
         }} />
         <Route exact path="/signup" render={props => {
           return(
-            <AuthForm {...props} buttonText="Sign me up!" heading="Join Chat-board today" signUp onAuth={authUser}/>
+            <AuthForm
+              {...props}
+              buttonText="Sign me up!"
+              heading="Join Chat-board today"
+              signUp onAuth={authUser}
+              errors={errors}
+              removeError={removeError}
+              />
           )
         }} />
       </Switch>
@@ -28,7 +42,8 @@ const Main = ({authUser}) => {
 function mapStateToProps(reduxState) {
   return {
     currentUser: reduxState.currentUser,
+    errors: reduxState.errors
   };
 }
 
-export default withRouter(connect(mapStateToProps, { authUser })(Main));
+export default withRouter(connect(mapStateToProps, { authUser, removeError })(Main));
--- chat-board-client/src/store/actions/auth.js
+++ chat-board-client/src/store/actions/auth.js
@@ -1,5 +1,6 @@
 import { apiCall } from "../../services/api";
 import { SET_CURRENT_USER } from "../actionTypes";
+import { addError, removeError } from "./errors";
 
 export function setCurrentUser(user) {
   return {
@@ -11,14 +12,17 @@ export function setCurrentUser(user) {
 export function authUser(type, userData) {
   return dispatch => {
     return new Promise((resolve, reject) => {
-      return apiCall("post", `/api/auth/${type}`, userData).then(({token, ...user}) => {
-        window.localStorage.setItem("jwtToken", token);
-        dispatch(setCurrentUser(user))
-        resolve();
-      })
-      .catch(e => {
-        console.log(e.message);
-      });
+      return apiCall("post", `/api/auth/${type}`, userData)
+        .then(({token, ...user}) => {
+          window.localStorage.setItem("jwtToken", token);
+          dispatch(setCurrentUser(user));
+          dispatch(removeError());
+          resolve();
+        })
+        .catch(err => {
+          dispatch(addError(err.message));
+          console.log(err.message);
+        });
     })
   }
 }
